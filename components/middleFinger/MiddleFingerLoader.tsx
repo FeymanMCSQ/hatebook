5a85e53247f4b4fd747f443aff1a98b0828807cb
Safi Ullah <150228504+FeymanMCSQ@users.noreply.github.com>
2025-10-04 18:48:05 +1000

    Made middle finger better

--- components/middleFinger/MiddleFingerLoader.tsx
+++ components/middleFinger/MiddleFingerLoader.tsx
@@ -4,13 +4,13 @@ import './MiddleFingerLoader.css';
 export default function MiddleFingerLoader() {
   return (
     <div className="hand">
-      <div className="finger f1" />
-      <div className="finger f2" />
+      <div className="finger side-finger f1" />
+      <div className="finger side-finger f2" />
       <div className="finger middle" />
 
       {/* Wrap f3 + thumb in a shared container */}
       <div className="finger-group">
-        <div className="finger f3" />
+        <div className="finger side-finger f3" />
         <div className="thumb" />
       </div>
     </div>
